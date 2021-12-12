4ae15bc416df33c09ddb036f0fb4edaee449c03d
Adony Ulloa <adonyulloa@gmail.com>
2021-12-12 12:57:43 -0600

    Programa de prueba consumir API

---
+++ Miapp/Municipio.cs
@@ -0,0 +1,12 @@
+public class Municipio
+{
+    public int municipioID{get; set;}
+
+    public string nombre {get;set;}
+
+    public string poblacion {get;set;}
+
+    public string latitud {get;set;}
+
+    public string longitud {get;set;}
+}
--- Miapp/Program.cs
+++ Miapp/Program.cs
@@ -1,26 +1,33 @@
-using System;
+﻿using System;
+using System.Net.Http;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace webAPIParcial
+namespace clientAPI
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            getMunicipio();
+            Console.ReadKey();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        static void getMunicipio()
+        {
+            ClientAPI client =new ClientAPI();
+            HttpResponseMessage httpResponseMessage = client.Find("Municipio").Result;
+            List<Municipio> municipio = httpResponseMessage.Content.ReadAsAsync<List<Municipio>>().Result;
+            Console.WriteLine("Municipio");
+
+            foreach(Municipio mun in municipio)
+            {
+                Console.WriteLine("ID: " + mun.municipioID);
+                Console.WriteLine("Nombre: " + mun.nombre);
+                Console.WriteLine("Poblacion: " + mun.poblacion);
+                Console.WriteLine("Latitud: " + mun.latitud);
+                Console.WriteLine("Longitud: " + mun.longitud);
+            }
+
+        }
     }
 }
